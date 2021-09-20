f5ab1f549b2fe0198906d4fd09c13ded26a6d08a
p22anishiyama <p22anishiyama@fairfieldprep.org>
2021-09-20 10:11:44 -0400

    submitting

---
+++ hw2-2.py
@@ -0,0 +1,13 @@
+# Author: ATN 9/20/21
+
+# Assigning values
+
+free_throws = 3
+basket = 6
+basket_three_point = 10
+
+# Final calculations
+
+points = (free_throws) + (basket * 2) + (basket_three_point * 3)
+
+print("The player scored " + str(points) + " points in the game.")
